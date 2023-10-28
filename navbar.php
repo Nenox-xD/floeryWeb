775777fee8c7dc233939bdba38e6f29ede6e6688
Nenox-xD <76703991+Nenox-xD@users.noreply.github.com>
2023-10-29 00:17:54 +0200

    php

---
+++ navbar.php
@@ -0,0 +1,23 @@
+<?php include 'navbar.php'; ?>
+<html>
+<head>
+<div class="nav-container">
+        <ul class="nav-links">
+          <li><a href="../html/index.html" class="nav-link active">Home</a></li>
+          <li class="center">
+            <a href="../html/portfolio.html" class="nav-link">Portfolio</a>
+          </li>
+          <li class="center">
+            <a href="../html/finances.html" class "nav-link">Finances</a>
+          </li>
+          <li class="center">
+            <a href="../html/aboutme.html" class="nav-link">About Me</a>
+          </li>
+          <li class="center">
+            <a href="../html/contact.html" class="nav-link">Contact</a>
+          </li>
+        </ul>
+      </div></head>
+<body>
+</body>
+</html>
